dabaf8dab8976088a90478ab02730c06b1818db6
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-05-23 17:53:03 -0500

    Simple Interest Calculato

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# A python script that calculates simple interest earned on an investment over a period of time.
+
+principal = 1000 # principal amount invested
+rate = 0.05 # annual interest rate for the investment of 5%
+time = 3 # number of years in the investment
+
+# Calculating the simple interest
+interest = principal * rate * time
+
+# Displaying the amount of interest earned
+print(f"The simple interest is: {interest}")
