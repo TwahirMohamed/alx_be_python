6910d04aeb1dc0e9a27ed2e54f4ddbe771b50c28
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-05-27 08:42:23 -0500

    changed variablle name for condition to weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,14 +2,14 @@
 # The task aims to demonstrate the use of if,elif and else statements to make decision in a program.
 
 # prompt user for weather input
-condition = input("What's the weather like today? (sunny/rainy/cold):")
+weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 # provide clothing recommendations
-if condition == "sunny":
+if weather == "sunny":
     message = "Wear a t-shirt and sunglasses."
-elif condition == "rainy":
+elif weather == "rainy":
     message = "Don't forget your umbrella and a raincoat."
-elif condition == "cold":
+elif weather == "cold":
     message = "Make sure to wear a warm coat and a scarf."
 else:
     message = "Sorry, I don't have recommendations for this weather."
