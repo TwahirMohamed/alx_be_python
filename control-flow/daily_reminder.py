471592321eef5c4da1bf8dabbd151db866b4fb76
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-05-30 20:28:44 -0500

    Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,18 @@
+# A script that will ask user for a single task, priority level and time-sensitiveness. 
+# The program will then provide a customized reminder for that particular task; demonstrating control flow and loops without relying on data structures to store multiple tasks.
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time bound? (yes/no): ")
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+        else:
+            print(f"Note:{task} is a {priority} task.\nConsider completing it when you have free time")
+
+
+    case "medium":
+        print(f"Note:{task} is a {priority} task.\nConsider completing it when you have free time")
+    case "low":
+        print(f"Note:{task} is a {priority} task.\nConsider completing it when you have free time")
+    
