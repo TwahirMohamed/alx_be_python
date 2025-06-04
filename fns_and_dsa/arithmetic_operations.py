e490678a0b4a8d85a8b2e98acda4ff4de22e55ec
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-06-03 19:18:03 -0500

    added division by zero scenerio

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -22,9 +22,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Division by zero is not possible."
+        else:
+            return num1 / num2
     else:
         return "Error: Unrecognized type of operation."
