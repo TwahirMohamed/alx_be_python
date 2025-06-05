c254d168fe0c9071a6ceea6274225fcb3d446722
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-06-05 16:16:58 -0500

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,41 @@
+# A python script that implements a simple interface for managing a shopping list.
+# The task focuses on lists to store and manipulate data dynamically.
+
+# Function for display menu
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+# Main program
+def main():
+    shopping_list = [] # Initializing with an empty list
+    # Loop for continous display
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case '1':
+                # Add an item
+                add_item = input("Please enter the item to add on the shopping list: ")
+                shopping_list.append(add_item)
+                pass
+            case '2':
+                # Remove an item
+                remove_item = input("Please enter the item to remove: ")
+                shopping_list.pop(remove_item)
+                pass
+            case '3':
+                # View List
+                print(shopping_list)
+                pass
+            case '4':
+                print("Goodbye!")
+                break
+            case _:
+                print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
