1728509e611c0094b69cd3aa48fce91cb763253f
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-06-05 18:56:56 -0500

    fixed the error message

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,7 +6,7 @@ def display_current_datetime():
 
 
 def calculate_future_date():
-    number_of_days = int(input("Enter number of days to add to the current date: "))
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.now() + timedelta(days = number_of_days)
     print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
 
