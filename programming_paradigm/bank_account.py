f9f612e017995dd3b1c35d6f371ea6d81dee62b5
TwahirMohamed <mohamedtwahir977@gmail.com>
2025-06-12 20:39:00 -0500

    Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    ''' Encapsulates Banking Operations'''
+    def __init__(self, account_balance=0):
+        self.__account_balance = account_balance
+
+    def deposit(self, amount):
+        ''' Method to handle deposit'''
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Amount deposited must be positive.")
+    def withdraw(self, amount):
+        ''' Method to handle withdrawal'''
+        if amount > self.__account_balance:
+            return False
+        elif amount <= 0:
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+    def display_balance(self):
+        ''' Method to display the account balance '''
+        print(f"Current account balance : ${self.__account_balance}")
