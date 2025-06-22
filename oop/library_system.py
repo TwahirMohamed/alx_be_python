691caaf4bd4f7a80966044b39dffb954655325c2
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-06-21 19:28:54 -0500

    added string method

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,17 +7,31 @@ class Book:
         self.title = title
         self.author = author
     
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"Book: {self.title} by {self.author}"
+    
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
     
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"EBook: {self.title} by {self.author}"
+    
+    
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+    
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"PrintBook: {self.title} by {self.author}"
+    
 
 class Library:
     def __init__(self):
